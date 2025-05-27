7f5c62331ad6ece92ad6d1feaf1c7b17145be54a
Cristianwds <cwalickidelossantos@udesa.edu.ar>
2025-05-27 10:57:05 -0300

    Creacion de ventana

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+import time
+import variables
+
+inicio= time.time()
+pygame.init()
+print("Hola mundo")
+
+#screen_info= pygame.display.get_desktop_sizes()
+#print(screen_info)
+modo_pantalla= True
+screen= pygame.display.set_mode((variables.ANCHO_VENTANA, variables.LARGO_VENTANA))
+run= True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run= False
+        if event.type== pygame.KEYDOWN:
+            if event.key== pygame.K_ESCAPE:
+                run= False
+    pygame.display.update()
+
+pygame.quit()
+final= time.time()
+print(f"Tiempo transcurrido: {final-inicio}")
